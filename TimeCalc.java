323384e975a97524d0e067c4520fc52e0b55dfcd
Ariel Yatskan <ariel.yatskan@post.runi.ac.il>
2025-11-06 18:12:48 +0200

    Finnished and working

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,6 +1,5 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
         int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
         int minuts = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         int newMinuts = (minuts + Integer.parseInt("" + args[1]));
