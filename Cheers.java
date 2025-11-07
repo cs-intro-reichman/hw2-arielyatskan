f2c5c9a961bea35e472531bef5a09a3bf11c7419
Ariel Yatskan <ariel.yatskan@post.runi.ac.il>
2025-11-07 12:05:19 +0200

    Finnished and working

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,21 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
+            String name = "" + args[0];
+            int cheerCount = Integer.parseInt(args[1]);
+            String lettersForAn = "AEFHILMNORSX";
+            for(int i = 0; i < name.length(); i++) {
+                char nameChar = name.charAt(i);
+                System.out.print("Give me ");
+                if(lettersForAn.indexOf(nameChar) != -1)  System.out.print("an ");
+                else  System.out.print("a  ");
+                System.out.println(nameChar + ": " + nameChar + "!");
+            }
+            System.out.println("What does that spell?");
+            while(cheerCount > 0) {
+                System.out.println(name + "!!!");
+                cheerCount--;
+            }
         }
 }
