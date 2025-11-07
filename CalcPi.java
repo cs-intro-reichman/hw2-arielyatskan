039101d6214e4c193e0d98e59f116472de14a6dc
Ariel Yatskan <ariel.yatskan@post.runi.ac.il>
2025-11-07 12:52:01 +0200

    Finnished and working

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,19 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+		int counter = Integer.parseInt(args[0]);
+		System.out.println("pi according to Java: " + Math.PI);
+		double sum = 0;
+		double denominator = 1;
+		boolean operator = true;
+		for(int i = 0; i < counter; i++) {
+			double quotient = 1 / denominator;
+			if(operator)  sum += quotient;
+			else sum -= quotient;
+			denominator += 2;
+			operator = !operator;
+		}
+		sum *= 4;
+		System.out.printf("pi, approximated:     " + sum);
 	}
 }
